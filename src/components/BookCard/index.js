7b6b366b689bcbc94b3b13d6b4743be00eacfa47
seekolugopi@gmail.com <seekolugopi@gmail.com>
2024-10-04 10:19:53 +0530

    add remove style

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -44,7 +44,7 @@ const BookCard=(props)=>{
                                 isButton && 
                                 <button className="remove-library" onClick={onRemoveFromCollection} >
                                     
-                                    {isAddLibrary?"Remove Book from Collection":""}
+                                    Remove Book from Collection
     
                                 </button>  // isAddLibrary &&
                             }
